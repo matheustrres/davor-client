5212f9a861648830b0d6113cf91b051dd8c0c62f
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-10 16:20:16 -0300

    feat: avoid multiple I/O calls && filter .{js,ts} files before loop

--- src/utils/load-resources.ts
+++ src/utils/load-resources.ts
@@ -9,25 +9,29 @@ type LoadResourcesProps<T> = {
 	path: string;
 };
 
-export const loadResources = <T>(props: LoadResourcesProps<T>): T[] => {
+export const loadResources = <T = unknown>(
+	props: LoadResourcesProps<T>,
+): T[] => {
 	const root = path.resolve(__dirname, '..');
-	const dirs = fs.readdirSync(path.join(root, props.path));
+	const baseDir = path.join(root, props.path);
 
-	for (const dir of dirs) {
-		const resources = fs.readdirSync(`${root}/${props.path}/${dir}`);
+	const directories = fs.readdirSync(baseDir);
+
+	for (const directory of directories) {
+		const dirPath = path.join(baseDir, directory);
+
+		const resources = fs
+			.readdirSync(dirPath)
+			.filter((file) => file.endsWith('.js') || file.endsWith('.ts'));
 
 		for (const resource of resources) {
-			if (resource.endsWith('.js') || resource.endsWith('.ts')) {
-				const resolvedPath = path.resolve(
-					`${root}/${props.path}/${dir}/${resource}`,
-				);
+			const resolvedPath = path.join(dirPath, resource);
 
-				// eslint-disable-next-line @typescript-eslint/no-var-requires
-				const Resource = require(resolvedPath).default;
-				const res = new Resource(props.client) as T;
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const Resource = require(resolvedPath).default;
+			const res = new Resource(props.client) as T;
 
-				props.resource.push(res);
-			}
+			props.resource.push(res);
 		}
 	}
 
